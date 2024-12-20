b547380d17c4c15d3e83aea701f29b0fe1ad22c1
Manya123-pavar <manyapower691@gmail.com>
2024-12-20 05:57:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<script>
-        function hamburg() {
-            const navbar = document.querySelector(".dropdown");
-            navbar.style.transform = "translateY(0px)";
-        }
-        function cancel() {
-            const navbar = document.querySelector(".dropdown");
-            navbar.style.transform = "translateY(-500px)";
-        }
-</script>
+console.log("Website loaded successfully!");
